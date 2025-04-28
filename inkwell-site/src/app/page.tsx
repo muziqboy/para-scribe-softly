4c8fdc90efd83db2223601e78f8a4e025118d6b4
Kaleb Girmay <kalebgirmay@Kalebs-MacBook-Pro.local>
2025-04-28 19:34:53 +0200

    new things for inkwell

---
+++ inkwell-site/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-semibold">
+        Welcome to <strong>Inkwell</strong>
+      </h1>
+    </main>
+  );
+}
